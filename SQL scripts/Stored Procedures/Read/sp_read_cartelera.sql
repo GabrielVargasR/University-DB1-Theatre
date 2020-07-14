d312f44c42cc00e704e7d5e5c0b5e18a81296f59
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-07-14 13:42:57 -0600

    added with check option to views

--- SQL scripts/Stored Procedures/Read/sp_read_cartelera.sql
+++ SQL scripts/Stored Procedures/Read/sp_read_cartelera.sql
@@ -7,12 +7,9 @@ CREATE PROCEDURE sp_read_cartelera(
 )
 
 BEGIN
-	-- estados posibles 2, 3, 4 (adelantada, anunciada, abierta)
-    DECLARE precios VARCHAR(255);
-    
-    SELECT pro.titulo, t.nombre, pre.fecha, f_get_precios_produccion(pro.id, t.id)
-    FROM Teatro AS t INNER JOIN (Produccion AS pro INNER JOIN Presentacion AS pre ON pre.id_produccion = pro.id) ON t.id = pro.id_teatro
-    WHERE (DATE(pre.fecha) BETWEEN pfecha_inicio AND pfecha_fin) AND pro.estado IN (2,3,4);
+    SELECT c.titulo, c.nombre, c.fecha, c.precios
+    FROM v_cartelera AS c
+    WHERE (DATE(c.fecha) BETWEEN pfecha_inicio AND pfecha_fin);
 END //
 
 DELIMITER ;
--- SQL scripts/Views/v_agentes_teatro.sql
+++ SQL scripts/Views/v_agentes_teatro.sql
@@ -1,3 +1,5 @@
-CREATE VIEW VAgentesTeatro AS
+DROP VIEW IF EXISTS v_agentes_teatro;
+CREATE VIEW v_agentes_teatro AS
 	SELECT a.cedula, a.nombre, a.fecha_nacimiento, a.sexo, a.direccion, a.tel_casa, a.celular, a.otro_tel, a.email, a.id_teatro, a.username
-    FROM Agente_Teatro AS a;
+    FROM Agente_Teatro AS a
+    WITH CHECK OPTION;
