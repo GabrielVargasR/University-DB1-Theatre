78af7261c8d5cbeb8a02b93a6272c7d1dcd39302
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-07-21 19:43:12 -0600

    Solved event issue

--- SQL scripts/Stored Procedures/Update/sp_update_registro_dia.sql
+++ SQL scripts/Stored Procedures/Update/sp_update_registro_dia.sql
@@ -5,11 +5,11 @@ BEGIN
 	DECLARE precio DECIMAL(8,2) DEFAULT 0;
     DECLARE cantidad INT DEFAULT 0;
     
-    SELECT AVG(r.precio), COUNT(*)
+    SELECT AVG(r.monto), COUNT(*)
     INTO precio, cantidad
     FROM Registro_Ventas AS r
-    WHERE r.fecha = CURRENDT_DATE() - INTERVAL 1 DAY;
+    WHERE DATE(r.fecha) = CURRENT_DATE() - INTERVAL 1 DAY;
     
-    INSERT INTO Registro_Dia(fecha, cantidad, costo_promedio) VALUES (CURRENDT_DATE() - INTERVAL 1 DAY, cantidad, precio);
+    INSERT INTO Registro_Dia(fecha, cantidad, costo_promedio) VALUES (CURRENT_DATE() - INTERVAL 1 DAY, cantidad, precio);
 END //
 DELIMITER ;
--- SQL scripts/event_reporte_diario.sql
+++ SQL scripts/event_reporte_diario.sql
@@ -2,8 +2,6 @@ SET GLOBAL event_scheduler = ON;
 
 CREATE EVENT reporte_diario
 ON SCHEDULE EVERY 1 DAY
-STARTS '2020-07-21 00:00:00'
+STARTS '2020-07-22 00:00:00'
 DO
 	CALL sp_update_registro_dia();
-    
-SHOW PROCESSLIST;
